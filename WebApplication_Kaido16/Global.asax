a136622302874cb1a9e8e2b118a4387a4294cc21
海藤 一臣 <qaswr.cdwef.8642@icloud.com>
2020-09-01 14:56:09 +0900

    First Commit

---
+++ WebApplication_Kaido16/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WebApplication_Kaido16.MvcApplication" Language="C#" %>
---
+++ WebApplication_Kaido16/Models/Book.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebApplication_Kaido16.Models
+{
+    public class Book
+    {
+        public int id { get; set; }
+        public string Title { get; set; }
+        public int Price { get; set; }
+        public string ISBN { get; set; }
+        public string PublishDate { get; set; }
+    }
+}
